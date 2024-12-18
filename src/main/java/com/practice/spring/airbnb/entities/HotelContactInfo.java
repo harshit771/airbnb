91858c3783b032c29f750ea957ff56f932c8c628
Harshit <harshittiwari771@gmail.com>
2024-12-18 22:08:09 +0530

    Project SetUp and creating entities

---
+++ src/main/java/com/practice/spring/airbnb/entities/HotelContactInfo.java
@@ -0,0 +1,5 @@
+package com.practice.spring.airbnb.entities;
+
+public class HotelContactInfo {
+
+}
---
+++ src/test/java/com/practice/spring/airbnb/AirBnbCloneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbCloneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
